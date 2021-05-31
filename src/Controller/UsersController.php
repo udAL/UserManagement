c23c1b830e7310350dc8b4a22a7302fefb027b91
Eudald Rossell Vivo <erossell@esolvo.es>
2021-05-31 17:43:20 +0200

    [ADDED] User - New, Edit, Remove

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,21 +2,129 @@
 
 namespace App\Controller;
 
-use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\Common\Cache\Psr6\InvalidArgument;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Security;
 
 class UsersController extends AbstractController
 {
+    private UserRepository $repository;
+    private EntityManagerInterface $manager;
+    private UserPasswordEncoderInterface $encoder;
+
+    public function __construct(
+        UserRepository $repository,
+        EntityManagerInterface $manager,
+        UserPasswordEncoderInterface $encoder)
+    {
+        $this->repository = $repository;
+        $this->manager = $manager;
+        $this->encoder = $encoder;
+    }
+
     /**
      * @Route("/admin/users", name="users")
      */
-    public function users(ManagerRegistry $registry): Response
+    public function users(): Response
     {
-        $users = $registry->getRepository(User::class)->findAll();
+        $users = $this->repository->findAll();
 
         return $this->render('admin/users.html.twig', ['users' => $users]);
+    }
+
+    /**
+     * @Route("/admin/user/new", name="user.new", methods={"GET"})
+     */
+    public function user_new(): Response
+    {
+        $user = new User();
+        return $this->render('admin/user.html.twig', ['user' => $user, 'error' => '']);
+    }
+
+    /**
+     * @Route("/admin/user/{id}", name="user.get", methods={"GET"})
+     */
+    public function user_get(int $id): Response
+    {
+        $user = $this->repository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException('User not found: '.$id);
+        }
+        else {
+            return $this->render('admin/user.html.twig', ['user' => $user, 'error' => '']);
+        }
+    }
+
+    /**
+     * @Route("/admin/user/new", name="user.new.post", methods={"POST"})
+     */
+    public function user_new_post(Request $request): Response
+    {
+        $user = new User();
+        $data = $request->request->all();
+        $user_same_email = $this->repository->findBy(['email' => $data['email']]);
+        if($user_same_email) {
+            throw new InvalidArgument('User email already exists');
+        }
+        else {
+            $user->setEmail($data['email']);
+            $user->setPassword(
+                $this->encoder->encodePassword($user, $data['password'])
+            );
+            $this->user_save($user, $data);
+
+            return $this->redirectToRoute('users');
+        }
+    }
+
+    /**
+     * @Route("/admin/user/{id}", name="user.post", methods={"POST"})
+     */
+    public function user_post(int $id, Request $request): Response
+    {
+        $user = $this->repository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'User not found: '.$id
+            );
+        }
+        else {
+            $this->user_save($user, $request->request->all());
+        }
+        return $this->redirectToRoute('users');
+    }
+
+    private function user_save(User $user, array $data)
+    {
+        $user->setName($data['name']);
+        $this->manager->persist($user);
+        $this->manager->flush();
+    }
+
+    /**
+     * @Route("/admin/user/delete/{id}", name="user.delete")
+     */
+    public function user_delete(int $id, Security $security): Response
+    {
+        $user = $this->repository->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'User not found: '.$id
+            );
+        }
+        else {
+            if($user->getUsername() != $security->getUser()->getUsername()) {
+                $this->manager->remove($user);
+                $this->manager->flush();
+            }
+        }
+        return $this->redirectToRoute('users');
     }
 }
